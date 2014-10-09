9ed76ab6032768dc7257b37c2fdb7ee11642bae5
Chris Hourihan <chourihan@gmail.com>
2014-10-08 22:46:15 -0400

    initial todos app

--- assets/js/main/todo/todo_controller.js
+++ assets/js/main/todo/todo_controller.js
@@ -12,6 +12,42 @@
     .controller('todoController', definitions);
 
   function todoController($scope) {
-    $scope.heading = 'To Do\'s!';
+    $scope.todos = initTodos();
+    $scope.addTodo = addTodo;
+    $scope.removeTodo = removeTodo;
+
+    function addTodo(newTodo) {
+      $scope.todos.push({
+        description: newTodo,
+        completed: false
+      });
+
+      $scope.newTodo = '';
+    }
+
+    function removeTodo(i) {
+      $scope.todos.splice(i, 1);
+    }
+
+    function initTodos() {
+      return [
+        {
+          description: 'Build a todo app',
+          completed: false
+        },
+        {
+          description: 'Teach angular',
+          completed: false
+        },
+        {
+          description: 'Take over the world',
+          completed: false
+        },
+        {
+          description: 'Feed Boomer the cat',
+          completed: true
+        }
+      ];
+    }
   }
 })(angular);
