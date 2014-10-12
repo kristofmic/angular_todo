2d41e4cf4321f489f5cc14b0961723153e203fed
Chris Hourihan <chourihan@gmail.com>
2014-10-11 20:20:51 -0400

    added todosService to demo factory vs service

--- assets/js/main/todos/todos.js
+++ assets/js/main/todos/todos.js
@@ -1,14 +1,20 @@
 (function(angular) {
 
   var
-    definitions;
+    factoryDefinitions,
+    serviceDefinitions;
 
-  definitions = [
+  factoryDefinitions = [
     todosFactory
   ];
 
+  serviceDefinitions = [
+    todosService
+  ];
+
   angular.module('ch.Main.Todos')
-    .factory('todos', definitions);
+    .factory('todos', factoryDefinitions)
+    .service('todosService', serviceDefinitions);
 
   function todosFactory() {
     var
@@ -49,36 +55,72 @@
         }
       });
     }
+  }
 
-    function data() {
-      return [
-        {
-          description: 'Build a todo app',
-          completed: false,
-          archived: false
-        },
-        {
-          description: 'Teach angular',
-          completed: false,
-          archived: false
-        },
-        {
-          description: 'Take over the world',
-          completed: false,
-          archived: false
-        },
-        {
-          description: 'Feed Boomer the cat',
-          completed: true,
-          archived: false
-        },
-        {
-          description: 'Clean Boomer\'s litter box',
-          completed: true,
-          archived: true
-        }
-      ];
+  function todosService() {
+    this.init = init;
+    this.create = create;
+    this.archive = archive;
+    this.destroy = destroy;
+    this.model = [];
+
+    function init() {
+      if (!this.model.length) {
+        angular.forEach(data(), function(td) {
+          this.model.push(td);
+        }, this);
+      }
     }
+
+    function create(newTodo) {
+      this.model.push({
+        description: newTodo,
+        completed: false,
+        archived: false
+      });
+    }
+
+    function archive(todo) {
+      todo.archived = true;
+    }
+
+    function destroy(todo) {
+      this.model.forEach(function (td, i) {
+        if (td === todo) {
+          this.model.splice(i, 1);
+        }
+      }, this);
+    }
+  }
+
+  function data() {
+    return [
+      {
+        description: 'Build a todo app',
+        completed: false,
+        archived: false
+      },
+      {
+        description: 'Teach angular',
+        completed: false,
+        archived: false
+      },
+      {
+        description: 'Take over the world',
+        completed: false,
+        archived: false
+      },
+      {
+        description: 'Feed Boomer the cat',
+        completed: true,
+        archived: false
+      },
+      {
+        description: 'Clean Boomer\'s litter box',
+        completed: true,
+        archived: true
+      }
+    ];
   }
 
 })(angular);
